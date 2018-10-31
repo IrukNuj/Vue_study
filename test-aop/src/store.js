8dae8c41a5610598fa503adae87fb36ea5d540f7
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-11-01 08:58:53 +0900

    vuex

--- test-aop/src/store.js
+++ test-aop/src/store.js
@@ -3,36 +3,52 @@ import Vue from 'vue'
 import Vuex from 'Vuex'
 Vue.use(Vuex)
 
-const store = new Vuex.Store({
+const moduleA = {
+  namespaced: true,
   state: {
-    count: 0,
-    list: [
-      { id: 1, name: 'りんご', price: 100 },
-      { id: 2, name: 'ばなな', price: 200 },
-      { id: 3, name: 'いちご', price: 300 }
-    ]
+    count: 1
   },
   mutations: {
-    mutationType(state, payload){
-      state.count = payload
+    update (state) {
+      state.count += 100
     }
+  }
+}
+
+const moduleB = {
+  namespaced: true,
+  state: {
+    count: 2
+  },
+  mutations: {
+    update (state) {
+      state.count += 200
+    }
+  }
+}
+
+const store = new Vuex.Store({
+  namespaced: true,
+  state: {
+    count: 0,
+    message: '初期メッセージ'
   },
   getters: {
-    count (state, getters, rootState, rootGetter) {
-      return state.count
-    },
-    max (state) {
-      return state.list.reduce((a, b) => {
-        return a.price > b.price ? a.price : b.price
-        // ここ a > b.price ? a :b.price でええのなんなん？
-      })
-    },
-    item (state) {
-      return id => state.list.find(el => el.id === id)
-    },
-    name (state,getters) {
-      return id => getters.item(id).name
+    message (state) { return state.message }
+  },
+  mutations: {
+    setMessage (state, payload) {
+      state.message = payload.message
     }
+  },
+  actions: {
+    doUpdate ({ commit }, message) {
+      commit('setMessage', { message })
+    }
+  },
+  modules: {
+    moduleA,
+    moduleB
   }
 })
 
